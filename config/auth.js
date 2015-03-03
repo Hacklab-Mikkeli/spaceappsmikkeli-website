7ef9134dd959417e215d4a6777a12a01769f5019
Belvain <heikki.kurhinen@gmail.com>
2015-03-03 17:08:07 +0200

    ** Added google authentication

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+﻿var auth = {
+  'google': {
+    'clientid': '274671629618-gi0lq5ng7ajeaff0q3rlloaplva40jjb.apps.googleusercontent.com',
+    'secret' : '81tCI8uSI3dF2Hch2A2sRXzF',
+    'callbackURL': 'http://localhost:3000/login/callback'
+   }
+};
+
+module.exports = auth;
