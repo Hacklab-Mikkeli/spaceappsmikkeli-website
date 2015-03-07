983610184f0fcd9ddffeccc96f9dea8bd1fe98ec
johannaraisa <johannaraisa@johannaraisambp.lan>
2015-03-07 13:43:32 +0200

    adds header image and footer styles

---
+++ public/js/responsive.js
@@ -0,0 +1,20 @@
+$(function () {
+  resizefunctions();
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
+$(window).load(function(){
+  jQuery.event.add(window, "resize", resizefunctions);
+});
+
+function resizefunctions(){
+  topHeight();
+}
+
+function topHeight() {
+  var height = $(window).height();
+  $(".jumbotron").height(height-300);
+
+  //var imgHeight = $(".top .image-wrapper").find("img").height();
+  //var textHeight = $(".top").find(".content").height()+100;
+  //$(".top").css("min-height", imgHeight+textHeight);
+};
