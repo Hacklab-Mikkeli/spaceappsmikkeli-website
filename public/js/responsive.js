fef661dde1fa95e8af5e466dde3e706cb1d15163
johannaraisa <johannaraisa@johannaraisambp.lan>
2015-03-08 11:04:29 +0200

    adds white space

--- public/js/responsive.js
+++ public/js/responsive.js
@@ -12,8 +12,4 @@ function resizefunctions(){
 function topHeight() {
   var height = $(window).height();
   $(".jumbotron").height(height-300);
-
-  //var imgHeight = $(".top .image-wrapper").find("img").height();
-  //var textHeight = $(".top").find(".content").height()+100;
-  //$(".top").css("min-height", imgHeight+textHeight);
 };
